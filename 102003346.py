9eabf4dcaf3a56144a77fc998b8711f020c1679e
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-01-21 02:07:24 +0530

    Update 102003346.py

--- 102003346.py
+++ 102003346.py
@@ -1,4 +1,30 @@
 import numpy as np
+from .topsis import normalize_matrix, calculate_relative_closeness, determine_final_ranking
+__version__ = '0.1'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/yourusername/topsis',
+    author='Vanshaj Singla',
+    author_email='vanshajrnv2002@gmail.com',
+    description='A python package for implementing the TOPSIS method',
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
+
 
 def normalize_matrix(matrix, impact):
     """
