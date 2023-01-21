32999294283bafa96800579671fcb0fac8a8873a
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-01-21 09:47:38 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/vanshajsingla/TOPSIS-PYTHON-PACKAGE/blob/main/102003346.py',
+    author='Vanshaj Singla',
+    author_email='vanshajrnv2002@gmail.com',
+    description='A python package for implementing the TOPSIS method',
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
