64d6f33af441a48a5ccb5c2eab8cfe2b57a40f49
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-01-21 17:55:16 +0530

    Update 102003346.py

--- 102003346.py
+++ 102003346.py
@@ -1,30 +1,114 @@
-import numpy as np
+import pandas as pd
+import os
+import sys
 
-def normalize_matrix(matrix, impact):
-    """
-    Normalize the decision matrix by dividing each element by the
-    square root of the sum of squares of the elements in the corresponding column
-    """
-    for i in range(matrix.shape[1]):
-        if impact[i] == "max":
-            matrix[:, i] = matrix[:, i] / np.sqrt(np.sum(np.square(matrix[:, i])))
-        else:
-            matrix[:, i] = -1 * matrix[:, i] / np.sqrt(np.sum(np.square(matrix[:, i])))
-    return matrix
+def main():
+    # Arguments not equal to 5
+    if len(sys.argv) != 5:
+        print("ERROR : NUMBER OF ARGUMENTS ARE NOT EQUAL TO 5")
+        exit(1)
 
-def calculate_relative_closeness(matrix, weights):
-    """
-    Calculate the relative closeness values for each alternative
-    """
-    weighted_matrix = np.multiply(matrix, weights)
-    relative_closeness = np.sum(weighted_matrix, axis=1)
-    return relative_closeness
+    # File Not Found error
+    elif not os.path.isfile(sys.argv[1]):
+        print(f"ERROR : {sys.argv[1]} Don't exist!!")
+        exit(1)
 
-def determine_final_ranking(matrix, weights, impact):
-    """
-    Determine the final ranking of the alternatives based on their relative closeness values
-    """
-    normalized_matrix = normalize_matrix(matrix, impact)
-    relative_closeness = calculate_relative_closeness(normalized_matrix, weights)
-    ranking = np.argsort(relative_closeness)
-    return ranking
+    # File extension not csv
+    elif ".csv" != (os.path.splitext(sys.argv[1]))[1]:
+        print(f"ERROR : {sys.argv[1]} is not csv!!")
+        exit(1)
+
+    else:
+        dataset, temp_dataset = pd.read_csv(
+            sys.argv[1]), pd.read_csv(sys.argv[1])
+        nCol = len(temp_dataset.columns.values)
+
+        # less then 3 columns in input dataset
+        if nCol < 3:
+            print("ERROR : Input file have less then 3 columns")
+            exit(1)
+
+        # Handling non-numeric values
+        for i in range(1, nCol):
+            pd.to_numeric(dataset.iloc[:, i], errors='coerce')
+            dataset.iloc[:, i].fillna(
+                (dataset.iloc[:, i].mean()), inplace=True)
+
+        # Handling errors of weighted and impact arrays
+        try:
+            weights = [int(i) for i in sys.argv[2].split(',')]
+        except:
+            print("ERROR : In weights array please check again")
+            exit(1)
+        impact = sys.argv[3].split(',')
+        for i in impact:
+            if not (i == '+' or i == '-'):
+                print("ERROR : In impact array please check again")
+                exit(1)
+
+        # Checking number of column,weights and impacts is same or not
+        if nCol != len(weights)+1 or nCol != len(impact)+1:
+            print(
+                "ERROR : Number of weights, number of impacts and number of columns not same")
+            exit(1)
+
+        if (".csv" != (os.path.splitext(sys.argv[4]))[1]):
+            print("ERROR : Output file extension is wrong")
+            exit(1)
+        if os.path.isfile(sys.argv[4]):
+            os.remove(sys.argv[4])
+        topsis_pipy(temp_dataset, dataset, nCol, weights, impact)
+
+
+def Normalize(temp_dataset, nCol, weights):
+    # normalizing the array
+    for i in range(1, nCol):
+        temp = 0
+        for j in range(len(temp_dataset)):
+            temp = temp + temp_dataset.iloc[j, i]**2
+        temp = temp**0.5
+        for j in range(len(temp_dataset)):
+            temp_dataset.iat[j, i] = (
+                temp_dataset.iloc[j, i] / temp)*weights[i-1]
+    return temp_dataset
+
+
+def Calc_Values(temp_dataset, nCol, impact):
+    # print(" Calculating Positive and Negative values...\n")
+    p_sln = (temp_dataset.max().values)[1:]
+    n_sln = (temp_dataset.min().values)[1:]
+    for i in range(1, nCol):
+        if impact[i-1] == '-':
+            p_sln[i-1], n_sln[i-1] = n_sln[i-1], p_sln[i-1]
+    return p_sln, n_sln
+
+
+def topsis_pipy(temp_dataset, dataset, nCol, weights, impact):
+    # normalizing the array
+    temp_dataset = Normalize(temp_dataset, nCol, weights)
+
+    # Calculating positive and negative values
+    p_sln, n_sln = Calc_Values(temp_dataset, nCol, impact)
+
+    # calculating topsis score
+    score = []
+    for i in range(len(temp_dataset)):
+        temp_p, temp_n = 0, 0
+        for j in range(1, nCol):
+            temp_p = temp_p + (p_sln[j-1] - temp_dataset.iloc[i, j])**2
+            temp_n = temp_n + (n_sln[j-1] - temp_dataset.iloc[i, j])**2
+        temp_p, temp_n = temp_p**0.5, temp_n**0.5
+        score.append(temp_n/(temp_p + temp_n))
+    dataset['Topsis Score'] = score
+
+    # calculating the rank according to topsis score
+    dataset['Rank'] = (dataset['Topsis Score'].rank(
+        method='max', ascending=False))
+    dataset = dataset.astype({"Rank": int})
+
+    # Writing the csv
+    dataset.to_csv(sys.argv[4], index=False)
+
+
+if __name__ == "__main__":
+    main()
