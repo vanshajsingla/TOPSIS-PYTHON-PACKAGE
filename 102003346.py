0444de8b23de893a18813ce2314c88d32b4c7aa8
vanshajsingla <72307225+vanshajsingla@users.noreply.github.com>
2023-01-21 09:46:56 +0530

    Update 102003346.py

--- 102003346.py
+++ 102003346.py
@@ -2,30 +2,6 @@ import numpy as np
 from .topsis import normalize_matrix, calculate_relative_closeness, determine_final_ranking
 __version__ = '0.1'
 
-from setuptools import setup, find_packages
-
-setup(
-    name='topsis',
-    version='0.1',
-    packages=find_packages(),
-    url='https://github.com/vanshajsingla/TOPSIS-PYTHON-PACKAGE/blob/main/102003346.py',
-    author='Vanshaj Singla',
-    author_email='vanshajrnv2002@gmail.com',
-    description='A python package for implementing the TOPSIS method',
-    install_requires=[
-        'numpy'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ]
-)
-
-
 def normalize_matrix(matrix, impact):
     """
     Normalize the decision matrix by dividing each element by the
